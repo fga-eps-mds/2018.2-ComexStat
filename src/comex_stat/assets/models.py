3654b0239d48cec279a89905a6b46dfad5df0d1e
Rogério Junior <junior.rogerio8@gmail.com>
2018-09-11 13:36:58 -0300

    Creating assets schema and models as a team in dojo
    Signed-off-by: Rogério Junior <junior.rogerio8@gmail.com>
    Co-authored-by: Caio Santos <caioiobs@hotmail.com>
    Co-authored-by: Kaique Borges <rique.kaique@gmail.com>
    Co-authored-by: João Victor <joao15victor08@gmail.com>
    Co-authored-by: Marcos Nery <marcosnery.comp@gmail.com>
    Co-authored-by: André Lucas <andre.lucas.lemos@hotmail.com>

--- src/comex_stat/assets/models.py
+++ src/comex_stat/assets/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-
+from datetime import datetime
 
 class AssetFacts(models.Model):
+    date = models.DateField(default=datetime.now)
+    origin = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
     class Meta:
         abstract = True
 
--- src/comex_stat/assets/schema.py
+++ src/comex_stat/assets/schema.py
@@ -3,3 +3,13 @@ import graphene
 from graphene_django.types import DjangoObjectType
 
 from comex_stat.assets.models import AssetImportFacts, AssetExportFacts
+
+class AssetImportFactsType(DjangoObjectType):
+    class Meta:
+        model = AssetImportFacts
+
+class Query(object):
+    all_import = graphene.List(AssetImportFactsType)
+
+    def resolve_all_import(self, info, **kwargs):
+        return AssetImportFacts.objects.all()
