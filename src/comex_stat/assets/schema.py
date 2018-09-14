526467c1002791a73e2ed1079fc7bd0404a5c2e9
Kaique Borges <rique.kaique@gmail.com>
2018-09-13 22:56:47 -0300

    Add AssetImportFacts and AssetImportFacts models in schema for graphene

--- src/comex_stat/assets/schema.py
+++ src/comex_stat/assets/schema.py
@@ -8,8 +8,16 @@ class AssetImportFactsType(DjangoObjectType):
     class Meta:
         model = AssetImportFacts
 
+class AssetExportFactsType(DjangoObjectType):
+    class Meta:
+        model = AssetExportFacts
+
 class Query(object):
     all_import = graphene.List(AssetImportFactsType)
+    all_export = graphene.List(AssetExportFactsType)
 
     def resolve_all_import(self, info, **kwargs):
         return AssetImportFacts.objects.all()
+
+    def resolve_all_export(self, info, **kwargs):
+        return AssetExportFacts.objects.all()
