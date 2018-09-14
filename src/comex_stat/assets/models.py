f51dddd0c4107985bbedba3b9367b5ea5fec5ce3
Kaique Borges <rique.kaique@gmail.com>
2018-09-13 22:23:38 -0300

    Add AssetImportFacts and AssetExportFacts models

--- src/comex_stat/assets/models.py
+++ src/comex_stat/assets/models.py
@@ -3,15 +3,26 @@ from datetime import datetime
 
 class AssetFacts(models.Model):
     date = models.DateField(default=datetime.now)
-    origin = models.CharField(max_length=100)
-    destination = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    ncm = models.CharField(max_length=250)
+    port = models.CharField(max_length=100)
+    port_code = models.CharField(max_length=100)
+    transportation = models.CharField(max_length=100)
+    registries = models.BigIntegerField()
+    net_kilogram = models.FloatField()
+    fob_value = models.FloatField()
+
     class Meta:
         abstract = True
 
 
 class AssetImportFacts(AssetFacts):
+    destination_fed_unit = models.CharField(max_length=100)
+    origin_country = models.CharField(max_length=100)
     pass
 
 
 class AssetExportFacts(AssetFacts):
+    origin_fed_unit = models.CharField(max_length=100)
+    destination_country = models.CharField(max_length=100)
     pass
