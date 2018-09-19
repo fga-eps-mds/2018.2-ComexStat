e6ef233556c440b117a9b98a5b1699b488788bef
Kaique Borges <rique.kaique@gmail.com>
2018-09-19 13:39:33 -0300

    Add CUCI model
    Signed-off-by: Kaique Borges <rique.kaique@gmail.com>
    Co-authored-by: André Lucas <andre.lucas.lemos@hotmail.com>

--- src/comex_stat/assets/models.py
+++ src/comex_stat/assets/models.py
@@ -2,12 +2,36 @@ from django.db import models
 from datetime import datetime
 
 
+class CUCI(models.Model):
+    cuci_item = models.CharField(max_length=250)
+    cuci_item_por = models.CharField(max_length=250)
+    cuci_item_ing = models.CharField(max_length=250)
+    cuci_item_esp = models.CharField(max_length=250)
+    cuci_sub = models.CharField(max_length=250)
+    cuci_sub_por = models.CharField(max_length=250)
+    cuci_sub_ing = models.CharField(max_length=250)
+    cuci_sub_esp = models.CharField(max_length=250)
+    cuci_pos = models.CharField(max_length=250)
+    cuci_pos_por = models.CharField(max_length=250)
+    cuci_pos_ing = models.CharField(max_length=250)
+    cuci_pos_esp = models.CharField(max_length=250)
+    cuci_cap = models.CharField(max_length=250)
+    cuci_cap_por = models.CharField(max_length=250)
+    cuci_cap_ing = models.CharField(max_length=250)
+    cuci_cap_esp = models.CharField(max_length=250)
+    cuci_sec = models.CharField(max_length=250)
+    cuci_sec_por = models.CharField(max_length=250)
+    cuci_sec_ing = models.CharField(max_length=250)
+    cuci_sec_esp = models.CharField(max_length=250)
+
+
 class NCM(models.Model):
     ncm = models.CharField(max_length=250)
     unid = models.CharField(max_length=100)
     ppe = models.CharField(max_length=100)
     ppi = models.CharField(max_length=100)
     fat_agreg = models.CharField(max_length=100)
+    cuci = models.ForeignKey(CUCI, on_delete=models.CASCADE)
     isic4 = models.CharField(max_length=100)
     exp_subset = models.CharField(max_length=100)
     ncm_por = models.CharField(max_length=100)
