ada1f807a859b54fb858036a418c5f8448d4961d
Marcos Nery <marcosnery.comp@gmail.com>
2018-09-20 13:15:48 -0300

    Creating assets relative models and its branch
    Signed-off-by: Marcos Nery <marcosnery.comp@gmail.com>
    Co-authored-by: Caio Santos <caioiobs@hotmail.com>

--- src/comex_stat/assets/models.py
+++ src/comex_stat/assets/models.py
@@ -16,13 +16,44 @@ class NCM(models.Model):
     siit = models.CharField(max_length=100)
 
 
+class TradeBlocs(models.Model):
+    bloc_name_pt = models.CharField(max_length=100)
+    bloc_name_en = models.CharField(max_length=100)
+    bloc_name_sp = models.CharField(max_length=100)
+    bloc_code = models.CharField(max_length=100)
+
+
+class Country(models.Model):
+    country_name_pt = models.CharField(max_length=100)
+    country_name_en = models.CharField(max_length=100)
+    country_name_sp = models.CharField(max_length=100)
+    country_code_iso3 = models.CharField(max_length=100)
+    trade_bloc = models.ForeignKey(TradeBlocs, on_delete=models.CASCADE)
+
+
+class FedUnit(models.Model):
+    uf_name = models.CharField(max_length=100)
+    uf_code = models.CharField(max_length=100)
+    uf_initials = models.CharField(max_length=100)
+
+
+class Transportation(models.Model):
+    transportation_name = models.CharField(max_length=100)
+    transportation_code = models.CharField(max_length=100)
+
+
+class Urf(models.Model):
+    urf_name = models.CharField(max_length=100)
+    urf_code = models.CharField(max_length=100)
+
+
 class AssetFacts(models.Model):
     date = models.DateField(default=datetime.now)
     name = models.CharField(max_length=100)
     ncm = models.ForeignKey(NCM, on_delete=models.CASCADE)
-    port = models.CharField(max_length=100)
-    port_code = models.CharField(max_length=100)
-    transportation = models.CharField(max_length=100)
+    urf = models.ForeignKey(Urf, on_delete=models.CASCADE, null=True)
+    transportation = models.ForeignKey(
+        Transportation, on_delete=models.CASCADE)
     registries = models.BigIntegerField()
     net_kilogram = models.FloatField()
     fob_value = models.FloatField()
@@ -32,12 +63,10 @@ class AssetFacts(models.Model):
 
 
 class AssetImportFacts(AssetFacts):
-    destination_fed_unit = models.CharField(max_length=100)
-    origin_country = models.CharField(max_length=100)
-    pass
+    destination_fed_unit = models.ForeignKey(FedUnit, on_delete=models.CASCADE)
+    origin_country = models.ForeignKey(Country, on_delete=models.CASCADE)
 
 
 class AssetExportFacts(AssetFacts):
-    origin_fed_unit = models.CharField(max_length=100)
-    destination_country = models.CharField(max_length=100)
-    pass
+    origin_fed_unit = models.ForeignKey(FedUnit, on_delete=models.CASCADE)
+    destination_country = models.ForeignKey(Country, on_delete=models.CASCADE)
