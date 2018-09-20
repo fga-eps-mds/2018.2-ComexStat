fa7b6d4a6f83da91ab80cc562e580cddabb25026
Kaique Borges <rique.kaique@gmail.com>
2018-09-20 17:02:51 -0300

    Add SH and CGCE models and correct idiom naming
    Signed-off-by: Kaique Borges <rique.kaique@gmail.com>
    Co-authored-by: André Lucas <andre.lucas.lemos@hotmail.com>

--- src/comex_stat/assets/schema.py
+++ src/comex_stat/assets/schema.py
@@ -6,6 +6,8 @@ from comex_stat.assets.models import AssetImportFacts
 from comex_stat.assets.models import AssetExportFacts
 from comex_stat.assets.models import NCM
 from comex_stat.assets.models import CUCI
+from comex_stat.assets.models import CGCE
+from comex_stat.assets.models import SH
 
 
 class AssetImportFactsType(DjangoObjectType):
@@ -28,11 +30,23 @@ class CUCIType(DjangoObjectType):
         model = CUCI
 
 
+class CGCEType(DjangoObjectType):
+    class Meta:
+        model = CGCE
+
+
+class SHType(DjangoObjectType):
+    class Meta:
+        model = SH
+
+
 class Query(object):
     all_import = graphene.List(AssetImportFactsType)
     all_export = graphene.List(AssetExportFactsType)
     all_ncm = graphene.List(NCMType)
     all_cuci = graphene.List(CUCIType)
+    all_cgce = graphene.List(CGCEType)
+    all_sh = graphene.List(SHType)
 
     def resolve_all_import(self, info, **kwargs):
         return AssetImportFacts.objects.all()
@@ -45,3 +59,9 @@ class Query(object):
 
     def resolve_all_cuci(self, info, **kwargs):
         return CUCI.objects.all()
+
+    def resolve_all_cgce(self, info, **kwargs):
+        return CGCE.objects.all()
+
+    def resolve_all_sh(self, info, **kwargs):
+        return SH.objects.all()
