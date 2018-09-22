511b3c3171523b0a3a47488a3f30188f2cd3bfa6
rogerioo <junior.rogerio8@gmail.com>
2018-09-22 00:10:16 -0300

    Setting schema as develop branch

--- src/comex_stat/assets/schema.py
+++ src/comex_stat/assets/schema.py
@@ -2,10 +2,7 @@ import graphene
 
 from graphene_django.types import DjangoObjectType
 
-from comex_stat.assets.models import AssetImportFacts
-from comex_stat.assets.models import AssetExportFacts
-from comex_stat.assets.models import NCM
-from comex_stat.assets.models import CUCI
+from comex_stat.assets.models import AssetImportFacts, AssetExportFacts, NCM
 
 
 class AssetImportFactsType(DjangoObjectType):
@@ -23,16 +20,10 @@ class NCMType(DjangoObjectType):
         model = NCM
 
 
-class CUCIType(DjangoObjectType):
-    class Meta:
-        model = CUCI
-
-
 class Query(object):
     all_import = graphene.List(AssetImportFactsType)
     all_export = graphene.List(AssetExportFactsType)
     all_ncm = graphene.List(NCMType)
-    all_cuci = graphene.List(CUCIType)
 
     def resolve_all_import(self, info, **kwargs):
         return AssetImportFacts.objects.all()
@@ -42,6 +33,3 @@ class Query(object):
 
     def resolve_all_ncm(self, info, **kwargs):
         return NCM.objects.all()
-
-    def resolve_all_cuci(self, info, **kwargs):
-        return CUCI.objects.all()
