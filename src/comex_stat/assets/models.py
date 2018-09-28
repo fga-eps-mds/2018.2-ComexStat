706bce6c03dd853714860577cd2272a81d49badf
Kaique Borges <rique.kaique@gmail.com>
2018-09-27 23:24:04 -0300

    Add tests and validation to have only numbers in code fields
    Signed-off-by: Kaique Borges <rique.kaique@gmail.com>
    Co-authored-by: João Victor <joao15victor08@gmail.com>

--- src/comex_stat/assets/models.py
+++ src/comex_stat/assets/models.py
@@ -1,42 +1,52 @@
 from django.db import models
+from django.core.validators import RegexValidator
 from datetime import datetime
+validate_only_numbers = RegexValidator(regex="^[0-9]+$",
+                                       message="Somente números são permitidos"
+                                       )
 
 
 class SH(models.Model):
     '''
         SH stands for Sistema Harmónico
     '''
-    chapter_code = models.CharField(max_length=2, blank=False)
-    chapter_name_pt = models.CharField(max_length=250, blank=False)
-    chapter_name_en = models.CharField(max_length=250, blank=False)
-    chapter_name_es = models.CharField(max_length=250, blank=False)
-    position_code = models.CharField(max_length=4, blank=False)
-    position_name_pt = models.CharField(max_length=250, blank=False)
-    position_name_en = models.CharField(max_length=250, blank=False)
-    position_name_es = models.CharField(max_length=250, blank=False)
-    subposition_code = models.CharField(max_length=6, blank=False)
-    subposition_name_pt = models.CharField(max_length=250, blank=False)
-    subposition_name_en = models.CharField(max_length=250, blank=False)
-    subposition_name_es = models.CharField(max_length=250, blank=False)
+    chapter_code = models.CharField(max_length=2, blank=True,
+                                    validators=[validate_only_numbers])
+    chapter_name_pt = models.CharField(max_length=250, blank=True)
+    chapter_name_en = models.CharField(max_length=250)
+    chapter_name_es = models.CharField(max_length=250)
+    position_code = models.CharField(max_length=4, blank=False,
+                                     validators=[validate_only_numbers])
+    position_name_pt = models.CharField(max_length=250)
+    position_name_en = models.CharField(max_length=250)
+    position_name_es = models.CharField(max_length=250)
+    subposition_code = models.CharField(max_length=6, blank=False,
+                                        validators=[validate_only_numbers])
+    subposition_name_pt = models.CharField(max_length=250)
+    subposition_name_en = models.CharField(max_length=250)
+    subposition_name_es = models.CharField(max_length=250)
     section_code = models.CharField(max_length=2, blank=False)
-    section_name_pt = models.CharField(max_length=250, blank=False)
-    section_name_en = models.CharField(max_length=250, blank=False)
-    section_name_es = models.CharField(max_length=250, blank=False)
+    section_name_pt = models.CharField(max_length=250)
+    section_name_en = models.CharField(max_length=250)
+    section_name_es = models.CharField(max_length=250)
 
 
 class CGCE(models.Model):
     '''
         CGCE stands for Classificação por Grandes Categorias Econômicas
     '''
-    level1_code = models.CharField(max_length=1, blank=False)
+    level1_code = models.CharField(max_length=1, blank=False,
+                                   validators=[validate_only_numbers])
     level1_name_pt = models.CharField(max_length=250, blank=False)
     level1_name_en = models.CharField(max_length=250, blank=False)
     level1_name_es = models.CharField(max_length=250, blank=False)
-    level2_code = models.CharField(max_length=2, blank=False)
+    level2_code = models.CharField(max_length=2, blank=False,
+                                   validators=[validate_only_numbers])
     level2_name_pt = models.CharField(max_length=250, blank=False)
     level2_name_en = models.CharField(max_length=250, blank=False)
     level2_name_es = models.CharField(max_length=250, blank=False)
-    level3_code = models.CharField(max_length=3, blank=False)
+    level3_code = models.CharField(max_length=3, blank=False,
+                                   validators=[validate_only_numbers])
     level3_name_pt = models.CharField(max_length=250, blank=False)
     level3_name_en = models.CharField(max_length=250, blank=False)
     level3_name_es = models.CharField(max_length=250, blank=False)
@@ -46,19 +56,23 @@ class CUCI(models.Model):
     '''
         CUCI stands for Classificação Uniforme do Comércio Internacional
     '''
-    item_code = models.CharField(max_length=5, blank=False)
+    item_code = models.CharField(max_length=5, blank=False,
+                                 validators=[validate_only_numbers])
     item_name_pt = models.CharField(max_length=250, blank=False)
     item_name_en = models.CharField(max_length=250, blank=False)
     item_name_es = models.CharField(max_length=250, blank=False)
-    subitem_code = models.CharField(max_length=4, blank=False)
+    subitem_code = models.CharField(max_length=4, blank=False,
+                                    validators=[validate_only_numbers])
     subitem_name_pt = models.CharField(max_length=250, blank=False)
     subitem_name_en = models.CharField(max_length=250, blank=False)
     subitem_name_es = models.CharField(max_length=250, blank=False)
-    position_code = models.CharField(max_length=3, blank=False)
+    position_code = models.CharField(max_length=3, blank=False,
+                                     validators=[validate_only_numbers])
     position_name_pt = models.CharField(max_length=250, blank=False)
     position_name_en = models.CharField(max_length=250, blank=False)
     position_name_es = models.CharField(max_length=250, blank=False)
-    chapter_code = models.CharField(max_length=2, blank=False)
+    chapter_code = models.CharField(max_length=2, blank=False,
+                                    validators=[validate_only_numbers])
     chapter_name_pt = models.CharField(max_length=250, blank=False)
     chapter_name_en = models.CharField(max_length=250, blank=False)
     chapter_name_es = models.CharField(max_length=250, blank=False)
@@ -72,16 +86,22 @@ class NCM(models.Model):
     '''
         NCM stands for Nomenclatura Comum do Mercosul
     '''
-    ncm_code = models.CharField(max_length=8, blank=False)
+    ncm_code = models.CharField(max_length=8, blank=False,
+                                validators=[validate_only_numbers])
     ncm_name_pt = models.CharField(max_length=250, blank=False)
     ncm_name_en = models.CharField(max_length=250, blank=False)
     ncm_name_es = models.CharField(max_length=250, blank=False)
-    statistic_unit_code = models.CharField(max_length=2, blank=False)
+    statistic_unit_code = models.CharField(max_length=2, blank=False,
+                                           validators=[validate_only_numbers])
     ppe_code = models.CharField(max_length=4, blank=False,
-                                verbose_name="Pauta de Produtos Exportados")
+                                verbose_name="Pauta de Produtos Exportados",
+                                validators=[validate_only_numbers])
     ppi_code = models.CharField(max_length=4, blank=False,
-                                verbose_name="Pauta de Produtos Importados")
-    aggregate_factor_code = models.CharField(max_length=1, blank=False)
+                                verbose_name="Pauta de Produtos Importados",
+                                validators=[validate_only_numbers])
+    aggregate_factor_code = models.CharField(max_length=1, blank=False,
+                                             validators=[validate_only_numbers]
+                                             )
     cuci = models.ForeignKey(CUCI, on_delete=models.CASCADE,
                              verbose_name='''Classificação Uniforme do
                                              Comércio Internacional''')
@@ -92,11 +112,13 @@ class NCM(models.Model):
                            verbose_name="Sistema Harmónico")
     isic4_code = models.CharField(max_length=2, blank=False,
                                   verbose_name='''International Standard Industrial
-                                               Classification (Revision 4)''')
+                                               Classification (Revision 4)''',
+                                  validators=[validate_only_numbers])
     exportation_subset = models.CharField(max_length=4, blank=False)
     siit_code = models.CharField(max_length=4, blank=False,
                                  verbose_name='''Setores Industriais por
-                                              Intensidade Tecnológica''')
+                                              Intensidade Tecnológica''',
+                                 validators=[validate_only_numbers])
 
 
 class TradeBlocs(models.Model):
--- src/comex_stat/assets/tests.py
+++ src/comex_stat/assets/tests.py
@@ -1,3 +1,73 @@
-# from django.test import TestCase
-
+from django.test import TestCase
+from comex_stat.assets.models import CUCI, NCM, SH, CGCE
+from django.core.exceptions import ValidationError
 # Create your tests here.
+
+
+class SHTests(TestCase):
+    def create_sh(self, chapter_code, position_code, subposition_code):
+        return SH.objects.create(chapter_code=chapter_code,
+                                 position_code=position_code,
+                                 subposition_code=subposition_code)
+
+    def test_code_fields_do_not_accept_letters(self):
+        """
+            Test if the fields that store codes does not accept letters or
+            special chars in it
+        """
+        sh = self.create_sh(chapter_code="a", position_code="4%3",
+                            subposition_code="1-+=3")
+        with self.assertRaises(ValidationError) as cm:
+            sh.full_clean()
+
+        self.assertTrue('chapter_code' in cm.exception.message_dict)
+        self.assertTrue('position_code' in cm.exception.message_dict)
+        self.assertTrue('subposition_code' in cm.exception.message_dict)
+
+
+class CGCETests(TestCase):
+    def create_cgce(self, level1_code, level2_code, level3_code):
+        return CGCE.objects.create(level1_code=level1_code,
+                                   level2_code=level2_code,
+                                   level3_code=level3_code)
+
+    def test_code_fields_do_not_accept_letters(self):
+        """
+            Test if the fields that store codes does not accept letters or
+            special chars in it
+        """
+        cgce = self.create_cgce(level1_code="2k",
+                                level2_code="d45",
+                                level3_code="skdfj")
+        with self.assertRaises(ValidationError) as cm:
+            cgce.full_clean()
+
+        self.assertTrue('level1_code' in cm.exception.message_dict)
+        self.assertTrue('level2_code' in cm.exception.message_dict)
+        self.assertTrue('level3_code' in cm.exception.message_dict)
+
+
+class CUCITests(TestCase):
+    def create_cuci(self, item_code, subitem_code,
+                    position_code, chapter_code):
+        return CUCI.objects.create(item_code=item_code,
+                                   subitem_code=subitem_code,
+                                   position_code=position_code,
+                                   chapter_code=chapter_code)
+
+    def test_code_fields_do_not_accept_letters(self):
+        """
+            Test if the fields that store codes does not accept letters or
+            special chars in it
+        """
+        cuci = self.create_cuci(item_code="2%",
+                                subitem_code="&*45",
+                                position_code="skdfj",
+                                chapter_code="test")
+        with self.assertRaises(ValidationError) as cm:
+            cuci.full_clean()
+
+        self.assertTrue('item_code' in cm.exception.message_dict)
+        self.assertTrue('subitem_code' in cm.exception.message_dict)
+        self.assertTrue('position_code' in cm.exception.message_dict)
+        self.assertTrue('chapter_code' in cm.exception.message_dict)
