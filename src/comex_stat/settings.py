43f4f04afb581b16763271606d80c63ce0bbba73
SannyaArvelos <sannyasantana@gmail.com>
2018-09-29 22:44:35 -0300

    Setting project_root path in the settings.py file.

--- src/comex_stat/settings.py
+++ src/comex_stat/settings.py
@@ -127,4 +127,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
