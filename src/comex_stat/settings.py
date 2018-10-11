9318637a5a43298db0429bf757253a68988bee04
Marcos Nery <marcosnery.comp@gmail.com>
2018-10-11 20:13:01 -0300

    Disabling csrf and adding CORS permission to same_origin access
    Signed-off-by: Marcos Nery <marcosnery.comp@gmail.com>

--- src/comex_stat/settings.py
+++ src/comex_stat/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'comex_stat.assets',
     'graphene_django',
+    'corsheaders',
 ]
 
 GRAPHENE = {
@@ -46,18 +47,22 @@ GRAPHENE = {
 }
 
 FIXTURE_DIRS = (
-   'assets/fixtures/',
+    'assets/fixtures/',
 )
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'comex_stat.urls'
 
