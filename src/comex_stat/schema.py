67d24c1c51ee966d87f3952f8457f580c28eed39
Marcos Nery <marcosnery.comp@gmail.com>
2018-11-18 16:54:36 -0200

    Adding graphiQL documentation
    Signed-off-by: Marcos Nery <marcosnery.comp@gmail.com>

--- src/comex_stat/schema.py
+++ src/comex_stat/schema.py
@@ -6,7 +6,8 @@ import comex_stat.assets.schema
 class Query(comex_stat.assets.schema.Query, graphene.ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
-    pass
+    class Meta:
+        description = "Essas são as operações possíveis de pesquisa e os respectivos campos que elas podem retornar. Abaixo de cada uma pode ser encontrada uma breve descrição sobre a sua função"
 
 
 schema = graphene.Schema(query=Query)
