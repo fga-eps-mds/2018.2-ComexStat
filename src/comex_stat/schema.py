2ca2e97005570de5f5a56dc5aa7cb3f797e1db1f
Marcos Nery <marcosnery.comp@gmail.com>
2018-11-19 03:36:57 -0200

    Adding graphiql documentation mention to API documentation
    Signed-off-by: Marcos Nery <marcosnery.comp@gmail.com>

--- src/comex_stat/schema.py
+++ src/comex_stat/schema.py
@@ -7,7 +7,9 @@ class Query(comex_stat.assets.schema.Query, graphene.ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
     class Meta:
-        description = "Essas são as operações possíveis de pesquisa e os respectivos campos que elas podem retornar. Abaixo de cada uma pode ser encontrada uma breve descrição sobre a sua função"
+        description = ("Essas são as operações possíveis de pesquisa e seus " +
+                       "respectivos filtros. Abaixo de cada uma pode ser" +
+                       "encontrada uma breve descrição sobre a sua função")
 
 
 schema = graphene.Schema(query=Query)
