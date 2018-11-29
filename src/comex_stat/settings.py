7e8852b0002adfb82b806aef38b9fa994164b7f6
SannyaArvelos <sannyasantana@gmail.com>
2018-11-29 11:19:42 -0200

    Fixing static files path.

--- src/comex_stat/settings.py
+++ src/comex_stat/settings.py
@@ -145,8 +145,8 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, '/static/')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'comex_stat/static/'),
-)
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+try:
+    django_heroku.settings(locals())
+except:
+    pass
